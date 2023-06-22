bef4a8824efed3a71fcbfc8a1bbb4e0e38018b77
Kaspah <casmir293@gmail.com>
2023-06-22 09:55:30 +0100

    interactive add to cart button

---
+++ js-folder/jumia-clone.js
@@ -0,0 +1,65 @@
+newDisplay = document.querySelector(".tablet-border");
+let productsHTML = "";
+
+products.forEach((product) => {
+  productsHTML += `<div class="my-product col-sm-4 col-lg-2">
+    <img
+      src="${product.image}"
+      class="product-image w-100"
+    />
+    <p class="product-name my-2">${product.name}</p>
+    <div class="d-flex align-items-center">
+      <img
+        src="images/ratings/rating-${product.rating.star * 10}.png"
+        loading="lazy"
+        class="rate-star h-auto me-3"
+      /><span class="text-primary">${product.rating.rateValue}</span>
+    </div>
+    <p class="my-1">&#8358; ${product.priceCent}</p>
+    <div>
+      <input
+        type="number"
+        value="1"
+        min="1"
+        max="99"
+        class="select-quantity px-1"
+      />
+    </div>
+    <button
+      class="add-to-cart js-add-to-cart text-light my-4 mx-auto mx-sm-0 d-flex
+      px-5 py-2 rounded shadow border-1" data-product-id="${product.id}"
+    >
+      Add to Cart
+    </button>
+    <div
+      class="mobile-border border-secondary border-1 border-bottom mb-4"
+    ></div>
+  </div>`;
+});
+
+newDisplay.innerHTML = productsHTML;
+
+let jsAddToCart = document.querySelectorAll(".js-add-to-cart");
+jsAddToCart.forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
---
+++ js-folder/products.js
@@ -0,0 +1,221 @@
+const products = [
+  {
+    id: "store-item-0001",
+    image: "images/products/art.jpg",
+    name: "Profile Sculpture",
+    rating: {
+      star: 4.5,
+      rateValue: 521,
+    },
+    priceCent: 35000,
+  },
+
+  {
+    id: "store-item-0002",
+    image: "images/products/basketball.jpg",
+    name: "Basket-Ball",
+    rating: {
+      star: 2.5,
+      rateValue: 12,
+    },
+    priceCent: 12500,
+  },
+
+  {
+    id: "store-item-0003",
+    image: "images/products/belt.jpg",
+    name: "Mens Belt",
+    rating: {
+      star: 3.5,
+      rateValue: 24,
+    },
+    priceCent: 6500,
+  },
+
+  {
+    id: "store-item-0004",
+    image: "images/products/chair.jpg",
+    name: "Outdoor/Recreation Chair",
+    rating: {
+      star: 4.0,
+      rateValue: 121,
+    },
+    priceCent: 23000,
+  },
+
+  {
+    id: "store-item-0005",
+    image: "images/products/chandalier.jpg",
+    name: "Luxury Chandalier",
+    rating: {
+      star: 5.0,
+      rateValue: 89,
+    },
+    priceCent: 62000,
+  },
+
+  {
+    id: "store-item-0006",
+    image: "images/products/chocolate.jpg",
+    name: "Double Carlbadi Chocolate",
+    rating: {
+      star: 4.0,
+      rateValue: 60,
+    },
+    priceCent: 1500,
+  },
+
+  {
+    id: "store-item-0007",
+    image: "images/products/comb.jpg",
+    name: "Comb Set",
+    rating: {
+      star: 3.5,
+      rateValue: 77,
+    },
+    priceCent: 3350,
+  },
+
+  {
+    id: "store-item-0008",
+    image: "images/products/cutlery.jpg",
+    name: "Set of Cutlery",
+    rating: {
+      star: 3.5,
+      rateValue: 152,
+    },
+    priceCent: 2800,
+  },
+
+  {
+    id: "store-item-0009",
+    image: "images/products/fan.jpg",
+    name: "Portable Fan",
+    rating: {
+      star: 4.5,
+      rateValue: 52,
+    },
+    priceCent: 8500,
+  },
+
+  {
+    id: "store-item-0010",
+    image: "images/products/flask.jpg",
+    name: "Fancy Flask",
+    rating: {
+      star: 4.0,
+      rateValue: 128,
+    },
+    priceCent: 3850,
+  },
+
+  {
+    id: "store-item-0011",
+    image: "images/products/flower.jpg",
+    name: "Flower Vase",
+    rating: {
+      star: 4.5,
+      rateValue: 12,
+    },
+    priceCent: 14000,
+  },
+
+  {
+    id: "store-item-0012",
+    image: "images/products/gift.jpg",
+    name: "Gift Pack",
+    rating: {
+      star: 3.5,
+      rateValue: 62,
+    },
+    priceCent: 1500,
+  },
+
+  {
+    id: "store-item-0013",
+    image: "images/products/headphone.jpg",
+    name: "JBL Headphone",
+    rating: {
+      star: 5.0,
+      rateValue: 115,
+    },
+    priceCent: 4500,
+  },
+
+  {
+    id: "store-item-0014",
+    image: "images/products/keyboard.jpg",
+    name: "White MacBook External Keyboard",
+    rating: {
+      star: 5.0,
+      rateValue: 141,
+    },
+    priceCent: 40000,
+  },
+
+  {
+    id: "store-item-0015",
+    image: "images/products/mouse.jpg",
+    name: "External Bluetooth Mouse",
+    rating: {
+      star: 3.0,
+      rateValue: 19,
+    },
+    priceCent: 8000,
+  },
+
+  {
+    id: "store-item-0016",
+    image: "images/products/mug.jpg",
+    name: "Glass Mug",
+    rating: {
+      star: 4.0,
+      rateValue: 228,
+    },
+    priceCent: 3650,
+  },
+
+  {
+    id: "store-item-0017",
+    image: "images/products/pen.jpg",
+    name: "Caigraphy Pen",
+    rating: {
+      star: 2.0,
+      rateValue: 8,
+    },
+    priceCent: 900,
+  },
+
+  {
+    id: "store-item-0018",
+    image: "images/products/pot.jpg",
+    name: "Small Size Pot",
+    rating: {
+      star: 3.5,
+      rateValue: 37,
+    },
+    priceCent: 7500,
+  },
+
+  {
+    id: "store-item-0019",
+    image: "images/products/soccer.jpg",
+    name: "FIFA Football",
+    rating: {
+      star: 2.0,
+      rateValue: 487,
+    },
+    priceCent: 13700,
+  },
+
+  {
+    id: "store-item-0020",
+    image: "images/products/speaker.jpg",
+    name: "JBL Flip 5 Speaker",
+    rating: {
+      star: 5.0,
+      rateValue: 337,
+    },
+    priceCent: 88000,
+  },
+];
